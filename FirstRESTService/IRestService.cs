0f8fff3a449b5ca1b2f7e5542629f87209eb415f
Weaveryxl <weaveryxl549@gmail.com>
2017-05-12 13:36:30 -0400

    FirstRestService

---
+++ FirstRESTService/IRestService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace FirstRESTService
+{
+  // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IRestService" in both code and config file together.
+  [ServiceContract]
+  public interface IRestService
+  {
+    [OperationContract]
+    [WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+    string CheckUserName(string name);
+  }
+}
